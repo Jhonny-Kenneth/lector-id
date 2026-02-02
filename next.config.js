189e3c089d9d3f1fc4d084268477c94fbf8f6782
Jhonny-Sousa-VC <jsousadossantos@mail.valenciacollege.edu>
2026-02-02 11:27:02 -0400

    cambios en el next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig = {
   reactStrictMode: true,
+  // Needed when the app is served from a subpath like /lectorid
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : "",
 };
 
 module.exports = nextConfig;
